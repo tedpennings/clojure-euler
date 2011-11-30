f41c60e818c5c5b501816fc371bb4daa08496e04
Ted Pennings <tedpennings@gmail.com>
2011-11-30 00:11:36 -0500

    cleaned up fizzbuzz (lol)

--- src/fizzbuzz.clj
+++ src/fizzbuzz.clj
@@ -1,10 +1,10 @@
 (defn fizzbuzz []
 	(loop [x 1]
-		(prn x)
-		(if (= 0 (rem x 3))
-		    (prn "Fizz"))
-		(if (= 0 (rem x 5))
-		    (prn "Buzz"))
-		(println)
+		(print (str x " "))
+		(if (zero? (mod x 3))
+		    (print "Fizz"))
+		(if (zero? (mod x 5))
+		    (print "Buzz"))
+		(println) ;only line break printed
 		(if (< x 100)
 			(recur (inc x)))))
